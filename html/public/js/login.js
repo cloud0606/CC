52a03237088cd91c42ca190af83a055a78617c6f
Wzy-CC <13651310509@126.com>
2019-07-09 01:37:58 +0800

    modify js function

--- html/public/js/login.js
+++ html/public/js/login.js
@@ -5,6 +5,16 @@ function update()
         document.getElementById("Phone-UN").innerHTML = "手机号登录";
         document.getElementById("exampleInputEmail1").placeholder = "Username";
         document.getElementById("exampleInputPassword1").placeholder = "Password";
+
+        // delete a button
+        var oldBtn = document.getElementById("exampleInputPassword1");
+        var parentNode = oldBtn.parentNode;
+        // 输入框的宽度还原为100%
+        document.getElementById("exampleInputPassword1").style.width = "100%";
+        var btn = document.getElementById("temp_varifycode_btn");
+        var btnp = btn.parentNode;
+        btnp.removeChild(btn);
+
         document.getElementById("exampleInputEmail1").name = "username";
     }
     else
@@ -12,12 +22,23 @@ function update()
         document.getElementById("Phone-UN").innerHTML = "账号登录";
         document.getElementById("exampleInputEmail1").placeholder = "Phone number";
         document.getElementById("exampleInputPassword1").placeholder = "Verify Code";
+        document.getElementById("exampleInputPassword1").style.width = "50%";
+        // create a button
+        var newBtn = document.createElement('button');
+        newBtn.id = "temp_varifycode_btn";
+        newBtn.setAttribute('class', 'btn btn-primary');
+        newBtn.style.width = "45%";
+        newBtn.style.height = '44px';
+        newBtn.style.margin = "0px 0px 5px 0px";
+        newBtn.type = "button";
+        newBtn.style.display = "inline";
+        newBtn.innerHTML = "发送验证码";
+        var oldBtn = document.getElementById("exampleInputPassword1");
+        var parentNode = oldBtn.parentNode;
+        parentNode.appendChild(newBtn);
+
         document.getElementById("exampleInputEmail1").name = "phone_number";
     }
  
 
-}
-
-function register(){
-    
 }
