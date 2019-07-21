c841d7afbe2a4f00dae4e981f3ea613a581037b6
cloud0606 <827594950@qq.com>
2019-07-21 19:44:40 +0800

    修复SQL注入漏洞

--- docker/html/src/object/order.php
+++ docker/html/src/object/order.php
@@ -20,9 +20,11 @@ class Order{
     function getOrderInfo($orderid,$userid) {
         try {
              $sql = "select orderid, userid, prodid, totalPrice,createtime from ".$this->table_name.
-            " where orderid=".$orderid." and userid='".$userid."'";
+            " where orderid=:orderid and userid=:userid ";
 	    $stmt = $this->conn->prepare($sql);
-            $stmt->execute();
+            $stmt->bindParam ( ':orderid', $orderid );	    
+	    $stmt->bindParam ( ':userid', $userid );
+	    $stmt->execute();
             $result =$stmt->fetch(PDO::FETCH_LAZY); // 查询单条数据
             return $result;
         } catch(PDOException $e) {
@@ -35,8 +37,9 @@ class Order{
         try {
             $sql = "insert into ".$this->table_name.
             " (userid, prodid,totalPrice) values (".
-            $userid.", ".$prodid.", ".$totalPrice.")";
-	        $stmt = $this->conn->prepare($sql);
+            $userid.", :prodid, ".$totalPrice.")";
+	    $stmt = $this->conn->prepare($sql);
+	    $stmt->bindParam ( ':prodid', $prodid );
             $stmt->execute();
             return $this->conn->lastInsertId(); // 返回订单ID
         } catch(PDOException $e) {
